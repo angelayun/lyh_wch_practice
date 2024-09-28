3a549154727e26ee640476ffbfa0766bcea17e70
liyunhua <liyunhua_angela@163.com>
2024-09-28 11:01:15 +0800

    feat(): add

--- src/.umi/testBrowser.tsx
+++ src/.umi/testBrowser.tsx
@@ -3,7 +3,7 @@
 // DO NOT CHANGE IT MANUALLY!
 import React, { useEffect, useState } from 'react';
 import { ApplyPluginsType } from 'umi';
-import { renderClient, RenderClientOpts } from '/Users/liyunhua/Documents/lyh/study/umi_practice/node_modules/@umijs/renderer-react';
+import { renderClient, RenderClientOpts } from '/Users/liyunhua/Documents/lyh/study/lyh_wch/lyh_wch_practice/node_modules/@umijs/renderer-react';
 import { createHistory } from './core/history';
 import { createPluginManager } from './core/plugin';
 import { getRoutes } from './core/route';
--- src/.umi/umi.ts
+++ src/.umi/umi.ts
@@ -3,7 +3,7 @@
 // DO NOT CHANGE IT MANUALLY!
 import './core/polyfill';
 
-import { renderClient } from '/Users/liyunhua/Documents/lyh/study/umi_practice/node_modules/@umijs/renderer-react';
+import { renderClient } from '/Users/liyunhua/Documents/lyh/study/lyh_wch/lyh_wch_practice/node_modules/@umijs/renderer-react';
 import { getRoutes } from './core/route';
 import { createPluginManager } from './core/plugin';
 import { createHistory } from './core/history';
